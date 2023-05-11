c2d920b408366c85a6854475728f0b3b68b51e75
Conzer <119615637+Contentzer0@users.noreply.github.com>
2023-05-11 14:42:16 -0400

    Conzer Seieieieieieieieieiei

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from peewee import *
 
-db = PostgresqlDatabase('people', user='banana', password='123',
+db = PostgresqlDatabase('contacts', user='banana', password='123',
                         host='localhost', port=5432)
 
 db.connect()
@@ -12,7 +12,23 @@ class BaseModel(Model):
 class Contacts(BaseModel):
     name = CharField()
     email = CharField()
-    phone_number = BigIntegerField
+    phone_number = BigIntegerField()
 
-guess = int(input("1: Add Contact\n2: View Contacts\n3: Search contacts by name"))
-
+guess = int(input("1: Add Contact\n2: View Contacts\n3: Search contacts by name\nSelect one : "))
+def function(guess):
+    if guess == 1:
+        name =input("Enter name: ")
+        email =input("Enter email :")
+        phone_number = int(input("Enter phone number :"))
+        new_contact = Contacts(name=name, email=email, phone_number=phone_number)
+        new_contact.save()
+    elif guess == 2:
+        contacts = Contacts.select()
+        for contact in contacts: 
+            print(contact.name, contact.email, contact.phone_number)
+    else:
+        search = input("A man needs a name :")
+        contacts = Contacts.select().where(Contacts.name == search)
+        for contact in contacts: 
+            print(contact.name, contact.email, contact.phone_number)
+function(guess)        
